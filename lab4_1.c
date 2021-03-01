797fac88182af9c2437cffc5e855be4358296856
autsadawut4869 <78350629+autsadawut4869@users.noreply.github.com>
2021-03-01 20:23:47 +0700

    edit

--- lab4_1.c
+++ lab4_1.c
@@ -1,33 +1,30 @@
 #include <stdio.h>
 int main()
 {
-    int num,i=0,j=0,max=0,min=2000;
-    int sum1=0,sum2=0,sum=0;
+    int num,i,j,m=0,mn=2000;
+    int km,kn,sum;
     scanf("%d",&num);
-    int metal[num][3];
-    for (i=0;i<num;i++)
-    {
-        for (j=0;j<3;j++)
-        {
+    int metal[num][3],x=num-2,y=i+3;
+    for (i=0;i<num;i++){
+        for (j=0;j<3;j++){
             scanf("%d",&metal[i][j]);
         }
     }
-    for(i=0;i<num-2;i++)
-    {
-        for(j=i;j<i+3;j++){
+    for(i=0;i<x;i++){
+        for(j=i;j<y;j++){
             sum=sum+(metal[j][0]*4)+(metal[j][1]*2)+(metal[j][2]*1);
         }
-        if(max<sum){
-           max=sum;
-           sum1=i+1;
+        if(m<sum){
+           m=sum;
+           km=i+1;
         }
-        if(min>sum){
-           min=sum;
-           sum2=i+1;
+        if(mn>sum){
+           mn=sum;
+           kn=i+1;
         }
         sum=0;
     }
-    printf("%d %d",sum1,sum2);
+    printf("%d %d",km,kn);
 
     return 0;
 }
